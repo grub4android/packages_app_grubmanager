c64c812dd8e14aebdfe43e21e9341dce46ef75fe
M1cha <sigmaepsilon92@gmail.com>
2015-01-04 13:48:47 +0100

    app: src: main: add ripple animation

--- app/src/main/java/org/grub4android/grubmanager/BootentryAdapter.java
+++ app/src/main/java/org/grub4android/grubmanager/BootentryAdapter.java
@@ -1,10 +1,13 @@
 package org.grub4android.grubmanager;
 
+import android.graphics.Color;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import com.balysv.materialripple.MaterialRippleLayout;
 
 import org.grub4android.grubmanager.models.Bootentry;
 
@@ -26,7 +29,6 @@ public class BootentryAdapter extends RecyclerView.Adapter<BootentryAdapter.View
         View v = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.list_item_bootentry, parent, false);
         ViewHolder vh = new ViewHolder(v);
-
 
         return vh;
     }
